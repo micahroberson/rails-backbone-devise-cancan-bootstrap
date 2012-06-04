e3c3da97d44aad2768384127dc24c5596b1698ea
Micah Roberson <Micah.roberson@gmail.com>
2012-06-04 12:37:07 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pm::Application.config.secret_token = '40f3b9e6f8ec8069886ed8a78c778ab255ae360c61ef90e479e853590f8df12cd60ce4e15dd794246f4030feac0d6d360e3c3d1d56310974667058f7dd8ed4ce'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Pm::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+
+  
+  root :to => "home#index"
+
+  devise_for :users
+end
